b7e511d5d19e2b2794bdaa16cd69c0d9f1026027
Anna Bertolani <bertolani.anna@gmail.com>
2019-05-23 01:05:42 +0200

    inizio modifiche HUD definitiva... si spera

--- Prototipo1/Assets/Scripts/HudUnitController.cs
+++ Prototipo1/Assets/Scripts/HudUnitController.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HudUnitController : MonoBehaviour
 {
-    public int UnitID;
+    public Image _image;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Zoom();
 	}
+
+    public void Zoom()
+    {
+        if (GameManager.singleton.sc.isActiveTank == true);
+        {
+            _image.rectTransform.sizeDelta = new Vector2(201.5f, 109.74f);
+        }
+    }
 }
