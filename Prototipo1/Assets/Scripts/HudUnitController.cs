83256f3413e418c042e06fcfb5adf51a75c282df
Anna Bertolani <bertolani.anna@gmail.com>
2019-06-06 12:43:49 +0200

    modifiche selezione pg (tank)

--- Prototipo1/Assets/Scripts/HudUnitController.cs
+++ Prototipo1/Assets/Scripts/HudUnitController.cs
@@ -11,6 +11,8 @@ public class HudUnitController : MonoBehaviour
     public Transform SlideBackTransform;
     public int UnitIndex;
     public GameObject InfoUnit;
+    public Slider LifeSlider;
+    public GameObject text;
 
     // Use this for initialization
     void Start ()
@@ -30,42 +32,58 @@ public class HudUnitController : MonoBehaviour
         if (GameManager.singleton.sc.isActiveTank == true && GameManager.singleton.sc.contSelectionP1 == 1 && UnitIndex == 1 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
         if (GameManager.singleton.sc.isActiveHealer == true && GameManager.singleton.sc.contSelectionP1 == 2 && UnitIndex == 2 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
         if (GameManager.singleton.sc.isActiveUtility == true && GameManager.singleton.sc.contSelectionP1 == 3 && UnitIndex == 3 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
         if (GameManager.singleton.sc.isActiveDealer == true && GameManager.singleton.sc.contSelectionP1 == 4 && UnitIndex == 4 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
 
         if (GameManager.singleton.sc2.isActiveTankP2 == true && GameManager.singleton.sc2.contSelectionP2 == 1 && UnitIndex == 1 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
         if (GameManager.singleton.sc2.isActiveHealerP2 == true && GameManager.singleton.sc2.contSelectionP2 == 2 && UnitIndex == 2 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
         if (GameManager.singleton.sc2.isActiveUtilityP2 == true && GameManager.singleton.sc2.contSelectionP2 == 3 && UnitIndex == 3 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
         if (GameManager.singleton.sc2.isActiveDealerP2 == true && GameManager.singleton.sc2.contSelectionP2 == 4 && UnitIndex == 4 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(1.20f, 1, 1), 0.9f);
+            text.SetActive(true);
             InfoUnit.SetActive(true);
         }
     }
@@ -75,42 +93,58 @@ public class HudUnitController : MonoBehaviour
         if (GameManager.singleton.sc.isActiveTank == false && GameManager.singleton.sc.contSelectionP1 == 1 && UnitIndex == 1 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
         if (GameManager.singleton.sc.isActiveHealer == false && GameManager.singleton.sc.contSelectionP1 == 2 && UnitIndex == 2 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
         if (GameManager.singleton.sc.isActiveUtility == false && GameManager.singleton.sc.contSelectionP1 == 3 && UnitIndex == 3 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
         if (GameManager.singleton.sc.isActiveDealer == false && GameManager.singleton.sc.contSelectionP1 == 4 && UnitIndex == 4 && GameManager.singleton.tm.isTurn == true && HUM.CanvasID == 1)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
 
         if (GameManager.singleton.sc2.isActiveTankP2 == false && GameManager.singleton.sc2.contSelectionP2 == 1 && UnitIndex == 1 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
         if (GameManager.singleton.sc2.isActiveHealerP2 == false && GameManager.singleton.sc2.contSelectionP2 == 2 && UnitIndex == 2 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
         if (GameManager.singleton.sc2.isActiveUtilityP2 == false && GameManager.singleton.sc2.contSelectionP2 == 3 && UnitIndex == 3 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
         if (GameManager.singleton.sc2.isActiveDealerP2 == false && GameManager.singleton.sc2.contSelectionP2 == 4 && UnitIndex == 4 && GameManager.singleton.tm.isTurn == false && HUM.CanvasID == 2)
         {
             transform.DOMove(SlideBackTransform.transform.position, 0.9f);
+            LifeSlider.transform.DOScale(new Vector3(0.6f, 0.6f, 1), 0.9f);
+            text.SetActive(false);
             InfoUnit.SetActive(false);
         }
     }
